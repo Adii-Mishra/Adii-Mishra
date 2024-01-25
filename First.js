e18a3777c9217b157923ba4caf028a3dc5ce5477
Aditya Narayan Mishra <91596724+Adii-Mishra@users.noreply.github.com>
2024-01-25 12:09:45 +0530

    Add files via upload

---
+++ First.js
@@ -0,0 +1,13 @@
+const instaId={
+    userName:"shradhakhapra",
+    isFollow: true,
+    posts: 195,
+    followers: 569000,
+    following: 4,
+    name: "Shradha Khapra",
+    xId :"shradhakhapra",
+    Job: "Enterpreneur",
+    Bio: "Apna College"
+
+};
+console.log(instaId);
